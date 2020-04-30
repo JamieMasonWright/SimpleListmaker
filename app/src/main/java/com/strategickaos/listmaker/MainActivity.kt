cf6077f900315c95132abe74b1c1a3c74600e17b
Jamie Mason_Wright <jamie.mason-wright@lush.co.uk>
2020-04-30 18:47:34 +0100

    read and write new list

--- app/src/main/java/com/strategickaos/listmaker/MainActivity.kt
+++ app/src/main/java/com/strategickaos/listmaker/MainActivity.kt
@@ -16,6 +16,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val listDataManager: ListDataManager = ListDataManager(this)
+
     lateinit var listsRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +29,10 @@ class MainActivity : AppCompatActivity() {
             showCreateListDialog()
         }
 
+      val lists = listDataManager.readLists()
         listsRecyclerView = findViewById(R.id.lists_recyclerView)
-
         listsRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,10 +51,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showCreateListDialog(){
+    private fun showCreateListDialog() {
+        // 1
         val dialogTitle = getString(R.string.name_of_list)
         val positiveButtonTitle = getString(R.string.create_list)
 
+        // 2
         val builder = AlertDialog.Builder(this)
         val listTitleEditText = EditText(this)
         listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
@@ -61,10 +64,18 @@ class MainActivity : AppCompatActivity() {
         builder.setTitle(dialogTitle)
         builder.setView(listTitleEditText)
 
-        builder.setPositiveButton(positiveButtonTitle){
-                dialog, _ ->
+        // 3
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            val list = TaskList(listTitleEditText.text.toString())
+            listDataManager.saveList(list)
+
+            val recyclerAdapter = listsRecyclerView.adapter as ListSelectionRecyclerViewAdapter
+            recyclerAdapter.addList(list)
+
             dialog.dismiss()
         }
+
+        // 4
         builder.create().show()
     }
 }
