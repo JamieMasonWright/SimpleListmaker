04475a2b5c577787ebe9b5e4de4a993a61a649f0
Jamie Mason_Wright <jamie.mason-wright@lush.co.uk>
2020-05-06 20:45:47 +0100

    addign tasklist

--- app/src/main/java/com/strategickaos/listmaker/ListDetailActivity.kt
+++ app/src/main/java/com/strategickaos/listmaker/ListDetailActivity.kt
@@ -2,19 +2,36 @@ package com.strategickaos.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_list_detail.*
 
 class ListDetailActivity : AppCompatActivity() {
 
-    lateinit var list: TaskList
+    lateinit var list : TaskList
+    lateinit var listItemsRecyclerView : RecyclerView
+    lateinit var addTaskButton : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_detail)
 
+        listItemsRecyclerView = findViewById(R.id.list_items_recyclerview)
+        listItemsRecyclerView .adapter = ListItemsRecyclerViewAdapter(list)
+        listItemsRecyclerView.layoutManager = LinearLayoutManager(this)
+
         list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)
 
         title = list.name
 
+        addTaskButton = findViewById(R.id.add_task_button)
+        addTaskButton.setOnClickListener{
+            showCreateTaskDialog()
+        }
+    }
+
+    private fun showCreateTaskDialog(){
 
     }
 }
